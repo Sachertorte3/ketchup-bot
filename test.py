ea53c179eff35c15e29d68f74d6ec0b7b02de08c
ice <nagarebosi120@gmail.com>
2022-05-13 23:48:13 +0900

    てｓｔ

--- test.py
+++
@@ -1,8 +0,0 @@
-with open('questions.txt', encoding="utf-8") as f:
-        lines = f.readlines()
-        questions = {}
-        for line in lines:
-            questions[line.split(',')[0]] = line.split(',')[1]
-        print([{"type": "postback", "data": f"Q:{question_Q}\nA:{question_A}", "label": question_Q}
-                         for question_Q, question_A in questions.items()])
-print(lines)
