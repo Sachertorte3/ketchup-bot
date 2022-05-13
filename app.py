f9937daa849ed85ea23064412a0bb04a8749aac4
ice <nagarebosi120@gmail.com>
2022-05-13 22:59:59 +0900

    test

--- app.py
+++ app.py
@@ -69,14 +69,15 @@ def make_select_message():
         questions = {}
         for line in lines:
             questions[line.split(',')[0]] = line.split(',')[1]
-        actionss=[{"type": "postback", "data": f"Q:{question_Q}\nA:{question_A}", "label": question_Q}
-                         for question_Q, question_A in questions.items()]
+        actions = []
+        for question_Q, question_A in questions.items():
+            actions.append({"type": "postback", "data": f"Q:{question_Q}\nA:{question_A}", "label": question_Q})
         return TemplateSendMessage(
             alt_text="選択肢",
             template=ButtonsTemplate(
                 title="よくある質問",
                 text="下から該当するものを選んでください。",
-                actions=actionss
+                actions=actions
             )
         )
 
